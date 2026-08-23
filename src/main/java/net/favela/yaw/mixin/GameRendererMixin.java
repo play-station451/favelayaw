888e732ebf0322e74ce793a85d7f9579d86156f5
play-station451 <playstation451discord@gmail.com>
2026-08-23 19:40:07 +0000

    stop crashing yo

--- src/main/java/net/favela/yaw/mixin/GameRendererMixin.java
+++ src/main/java/net/favela/yaw/mixin/GameRendererMixin.java
@@ -2,6 +2,7 @@ package net.favela.yaw.mixin;
 
 import net.favela.yaw.impl.modules.categories.render.FullBright;
 import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class GameRendererMixin {
 
     @Inject(method = "nightVisionScale", at = @At("HEAD"), cancellable = true)
-    private void yaw$onNightVisionScale(CallbackInfoReturnable<Float> cir) {
+    private static void yaw$onNightVisionScale(LivingEntity entity, float partialTick, CallbackInfoReturnable<Float> cir) {
         if (FullBright.INSTANCE != null && FullBright.INSTANCE.isPotionsMode()) {
             cir.setReturnValue(1.0F);
         }
