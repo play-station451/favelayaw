e32754d3a26a8c6e29001ec8bef367c01223db2f
play-station451 <playstation451discord@gmail.com>
2026-08-23 19:09:25 +0000

    SFDA

--- src/main/java/net/favela/yaw/mixin/LightTextureMixin.java
+++ src/main/java/net/favela/yaw/mixin/LightTextureMixin.java
@@ -1,8 +1,10 @@
 package net.favela.yaw.mixin;
 
 import net.favela.yaw.impl.modules.categories.render.FullBright;
-import net.minecraft.client.renderer.LightTexture;
+import net.minecraft.core.Holder;
+import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,12 +12,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import static net.favela.yaw.impl.util.wrapper.Wrapper.MC;
 
-@Mixin(LightTexture.class)
-public abstract class LightTextureMixin {
+@Mixin(LivingEntity.class)
+public abstract class LivingEntityMixin {
 
-    @Inject(method = "hasNightVision", at = @At("HEAD"), cancellable = true)
-    private void yaw$onHasNightVision(CallbackInfoReturnable<Boolean> cir) {
-        if (FullBright.INSTANCE != null && FullBright.INSTANCE.isPotionsMode()) {
+    @Inject(method = "hasEffect", at = @At("HEAD"), cancellable = true)
+    private void yaw$onHasEffect(Holder<MobEffect> effect, CallbackInfoReturnable<Boolean> cir) {
+        if (MC.player == null || (Object) this != MC.player) return;
+        if (FullBright.INSTANCE == null || !FullBright.INSTANCE.isPotionsMode()) return;
+        if (effect.is(MobEffects.NIGHT_VISION)) {
             cir.setReturnValue(true);
         }
     }
