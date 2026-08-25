fbbbd813135fa2a93b37582cdd305ba896558559
play-station451 <playstation451discord@gmail.com>
2026-08-25 12:04:47 -0400

    Create MultiPlayerGameModeAccessor.java

---
+++ src/main/java/net/favela/yaw/mixin/MultiPlayerGameModeAccessor.java
@@ -0,0 +1,17 @@
+package net.favela.yaw.mixin;
+
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.multiplayer.MultiPlayerGameMode;
+import net.minecraft.client.multiplayer.prediction.PredictiveAction;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(MultiPlayerGameMode.class)
+public interface MultiPlayerGameModeAccessor {
+
+    @Invoker("startPrediction")
+    void favelayaw$startPrediction(
+            ClientLevel level,
+            PredictiveAction action
+    );
+}
